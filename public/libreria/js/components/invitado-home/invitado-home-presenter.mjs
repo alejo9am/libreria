04e6ebc650ed967038345784b9fd5d784921340d
vvvalero <vvaleroj6@gmail.com>
2025-10-15 13:45:25 +0200

    He añadido la funcionalidad de que al abrir la página principal como invitado (index.html), muestre directamente el catálogo sin tener que redirigir a catalogo.html

--- public/libreria/js/components/invitado-home/invitado-home-presenter.mjs
+++ public/libreria/js/components/invitado-home/invitado-home-presenter.mjs
@@ -1,11 +1,26 @@
 import { Presenter } from "../../commons/presenter.mjs";
+import { InvitadoCatalogoLibroPresenter } from "../invitado-catalogo-libro/invitado-catalogo-libro-presenter.mjs";
 
 export class InvitadoHomePresenter extends Presenter {
   constructor(model, view, parentSelector) {
     super(model, view, parentSelector);
   }
 
+  get catalogoElement() {
+    return document.querySelector("#catalogo");
+  }
   async refresh() {
-    super.refresh();
+    await super.refresh();
+    let libros = this.model.getLibros();
+
+    await Promise.all(
+      libros.map(async (l) => {
+        return await new InvitadoCatalogoLibroPresenter(
+          l,
+          "invitado-catalogo-libro",
+          "#catalogo"
+        ).refresh();
+      })
+    );
   }
 }
