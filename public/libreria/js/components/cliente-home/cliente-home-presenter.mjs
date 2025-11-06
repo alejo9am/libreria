be30f6b41b024c04065281490cba3234e961cb8b
Alejo Arenas Moreira <arenasmoreira@gmail.com>
2025-11-06 20:28:00 +0100

    Fix de los mensajes del cliente

--- public/libreria/js/components/cliente-home/cliente-home-presenter.mjs
+++ public/libreria/js/components/cliente-home/cliente-home-presenter.mjs
@@ -1,5 +1,8 @@
 import { Presenter } from "../../commons/presenter.mjs";
 import { ClienteCatalogoLibroPresenter } from "../cliente-catalogo-libro/cliente-catalogo-libro-presenter.mjs";
+import { LibreriaSession } from "../../commons/libreria-session.mjs";
+import { router } from "../../commons/router.mjs";
+import { renderUltimoMensaje } from "../../commons/mensajes-helper.mjs";
 
 export class ClienteHomePresenter extends Presenter {
   constructor(model, view, parentSelector) {
@@ -12,6 +15,36 @@ export class ClienteHomePresenter extends Presenter {
   async refresh() {
     await super.refresh();
 
+    // Obtener la URL anterior del router (SIN usar document.referrer)
+        const previousUrl = router.previousUrl || '';
+    
+        // Páginas del navbar
+        const noMostrarMensaje = ['cliente-carrito.html'];
+        const vieneDeNoMostrar = noMostrarMensaje.some(pagina => previousUrl.includes(pagina));
+
+        console.log('[CarritoPresenter] URL anterior:', previousUrl);
+        console.log('[CarritoPresenter] Viene de no mostrar:', vieneDeNoMostrar);
+
+        if (mensajesContainer) {
+          //si viene de nomostrar o si el contenido del mensaje es cantidad de libros agregado al carrito, no mostrar el mensaje
+          if (vieneDeNoMostrar || mensajesContainer.innerHTML.includes("Cantidad")) {
+            // Limpiar mensajes si venimos de no mostrar
+            LibreriaSession.clearMessages();
+            mensajesContainer.innerHTML = "";
+          } else {
+            // Mostrar mensajes si venimos de otra acción (ej: añadir al carrito)
+            renderUltimoMensaje("#mensajesContainer");
+          }
+        }
+    
+    // Verificar si el usuario es cliente, sino redirigir al login
+    const userSession = LibreriaSession.getUserSession();
+    if (!userSession || userSession.rol !== "CLIENTE") {
+      LibreriaSession.addMessage("error", "Debe iniciar sesión como cliente");
+      console.log("ERROR, usuario no autorizado", userSession);
+      router.navigate("/libreria/invitado-ingreso.html");
+      return;
+    }
   
     let libros = this.model.getLibros();
 
@@ -24,5 +57,16 @@ export class ClienteHomePresenter extends Presenter {
         ).refresh();
       })
     );
+
+    // Cerrar sesion
+    const salirLink = document.getElementById("salirLink");
+    if (salirLink) {
+      salirLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        LibreriaSession.clearUserSession();
+        LibreriaSession.addMessage("success", "Sesión cerrada correctamente");
+      });
+    }
+
   }
 }
